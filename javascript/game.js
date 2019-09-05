b499c41b26a4b235dcd8e037d80f01032738620e
Jorge Luis <jorge.luis4212@gmail.com>
2019-09-05 13:18:09 -0400

    6 commit

--- javascript/game.js
+++ javascript/game.js
@@ -9,7 +9,8 @@ const GAMESTATE = {
     RUNNING: 1,
     MENU: 2,
     GAMEOVER: 3,
-    NEWLEVEL: 4
+    NEWLEVEL: 4,
+    GAMEWON: 5
 }
 export default class Game {
     constructor(gamewidth, gameheight) {
@@ -21,7 +22,6 @@ export default class Game {
         this.gameObjects = [];
         this.bricks = [];
         this.lives = 3;
-        //  document.getElementById('livesLeft').innerHTML = this.lives
 
         this.levels = [level1, level2];
         this.currentlevel = 0;
@@ -44,12 +44,14 @@ export default class Game {
     }
     update(deltaTime) {
         if (this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER
-        if (this.gamestate === GAMESTATE.PAUSED || this.gamestate === GAMESTATE.MENU || this.gamestate === GAMESTATE.GAMEOVER) return;
+        if (this.gamestate === GAMESTATE.PAUSED || this.gamestate === GAMESTATE.MENU || this.gamestate === GAMESTATE.GAMEOVER || this.gamestate=== GAMESTATE.GAMEWON) return;
 
-        if (this.bricks.length === 0) {
+        if (this.bricks.length === 0 && this.currentlevel === 0 ) {
             this.currentlevel++;
             this.gamestate = GAMESTATE.NEWLEVEL;
             this.start();
+        } else if (this.bricks.length === 0 && this.currentlevel === 1 ) {
+            this.gamestate = GAMESTATE.GAMEWON
         }
         [...this.gameObjects, ...this.bricks]
             .forEach(object => object.update(deltaTime));
@@ -70,20 +72,33 @@ export default class Game {
         if (this.gamestate === GAMESTATE.MENU) {
             var startimg = document.getElementById("start_img");
             var start = ctx.createPattern(startimg, "repeat");
-            ctx.rect(1, 1, this.gamewidth, this.gameheight);
+            ctx.rect(0, 0, this.gamewidth, this.gameheight);
             ctx.fillStyle = start;
             ctx.fill();
         }
         if (this.gamestate === GAMESTATE.GAMEOVER) {
             var overimg = document.getElementById("gameover_img");
             var over = ctx.createPattern(overimg, "repeat");
-            ctx.rect(1, 1, this.gamewidth, this.gameheight);
+            ctx.rect(0, 0, this.gamewidth, this.gameheight);
             ctx.fillStyle = over;
             ctx.fill();
             ctx.font = "30px Times New Roman";
             ctx.fillStyle = "black";
             ctx.textAlign = "left";
             ctx.fillText("🐰GAMEOVER🐰", this.gamewidth / 3, this.gameheight / 2)
+            document.getElementById('playAgain').style.display = "block"
+        }
+        if(this.gamestate === GAMESTATE.GAMEWON ){
+            var wonimg = document.getElementById("win_img");
+            var won = ctx.createPattern(wonimg, "repeat");
+            ctx.rect(0, 0, this.gamewidth, this.gameheight);
+            ctx.fillStyle = won;
+            ctx.fill();
+            ctx.font = "50px Times New Roman";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "left";
+            ctx.fillText("🐰GAMEWON🐰", this.gamewidth / 4, this.gameheight / 2)
+            document.getElementById('playAgain').style.display = "block"
         }
     }
 
